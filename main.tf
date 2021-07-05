6add0be018db3a422212217ad0842161623cc2d6
Muller, Miklos <miklos.muller@lhsystems.com>
2021-07-05 10:48:32 +0200

    update

--- main.tf
+++ main.tf
@@ -10,26 +10,35 @@ provider "statuspage" {
   api_key = "a4f8fd27-2eed-4c71-b101-9580c56bcd18"
 }
 
-data "statuspage_components" "default" {
+#data "statuspage_components" "default" {
+#    page_id = "wvy1y3msjr06"
+##    filter {
+##        name = "name"
+##        values = [ "value_1", "value_2" ]
+##    }
+#}
 
-    page_id = local.page_id
+#output "statuspage_components" {
+#	value = jsonencode(data.statuspage_components.default.components)
+#}
 
+data "statuspage_component_groups" "default" {
+    page_id = "wvy1y3msjr06"
 #    filter {
 #        name = "name"
 #        values = [ "value_1", "value_2" ]
 #    }
 }
 
-output "statuspage_components" {
-	value = jsonencode(data.statuspage_components.default.components)[0]
+output "statuspage_component_groups" {
+	value = jsonencode(data.statuspage_component_groups.default.component_groups)
 }
 
 #resource "statuspage_component" "my_component" {
-#  page_id     = "029l93xg1bxd"
+#  page_id     = "wvy1y3msjr06"
 #  name        = "Mikloska homokozoja 6"
 #  description = "see TCOE-593 for more info"
 #  status      = "operational"
-#
 #  lifecycle {
 #      ignore_changes = [
 #          status
@@ -37,24 +46,15 @@ output "statuspage_components" {
 #  }
 #}
 
-#resource "statuspage_incident" "my_incident" {
-#  page_id     = "029l93xg1bxd"
-#
-#  name    = "Test incident name 5"
-#  impact_override = "none"
-#  status = "investigating"
-#  body   = "We are currently investigating the issue."
-
-#  component {
-#    id = statuspage_component.my_component.id
-#    name = statuspage_component.my_component.name
-#    status = "under_maintenance"
-#  }
-#
-#  component {
-#    id = statuspage_component.my_component_2.id
-#    name = statuspage_component.my_component_2.name
-#    status = "under_maintenance"
-#  }
-
+resource "statuspage_incident" "my_incident" {
+  page_id     = "wvy1y3msjr06"
+  name    = "Test incident name 6"
+  impact_override = "none"
+  status = "investigating"
+  body   = "We are currently investigating the issue."
+  component {
+    id = statuspage_component.my_component.id
+    name = statuspage_component.my_component.name
+    status = "under_maintenance"
+  }
 }
